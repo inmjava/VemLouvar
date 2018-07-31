52cfe55ab33abae43dbf8b6875494d6fe4ff88a4
Ivan <inmjava@yahoo.com.br>
2018-07-31 00:29:23 -0300

    tentando algo mais com fx

--- src/main/java/br/com/ivan/missagenerator/frame/MenuPrincipalFX.java
+++ src/main/java/br/com/ivan/missagenerator/frame/MenuPrincipalFX.java
@@ -1,12 +1,17 @@
 package br.com.ivan.missagenerator.frame;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MenuPrincipalFX extends Application {
+	
+	@FXML
+	private TextField txtPesquisa;
 
 	public static void main(String[] args) {
 		launch();
@@ -15,7 +20,7 @@ public class MenuPrincipalFX extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/vemlouvarfreeform.fxml"));
-
+		
 		Scene scene = new Scene(root, 853, 597);
 
 		stage.setTitle("FXML Welcome");
